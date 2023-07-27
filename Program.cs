699c1103a7ff936b43a82ac838278111542a0226
Piktcher <pavelordenov@yandex.ru>
2023-07-27 21:17:07 +0200

    Added 1st iteration of the program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿void CopyElementstoArray(string[] array)
+{
+    string[] array_new = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array_new[i] = array[i];
+        }
+    }
+
+    for (int i = 0; i < array_new.Length; i++)
+    {
+        Console.Write(array_new[i] + ' ');
+    }
+}
+
+string[] array = {"1234", "1567", "-2", "computer science"};
+CopyElementstoArray(array);
